99ffac6e61d4160dda8e611e28751052d89b96f7
Jeff Cohen <jeff@beeswax.com>
2019-04-18 12:13:09 -0500

    connect mysql db

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # kosher_connections
+
+1. Clone this repo
+2. Set up a virtual env using Python 3.6
+```
+brew unlink python # ONLY if you have installed (with brew) another version of python 5
+brew install --ignore-dependencies https://raw.githubusercontent.com/Homebrew/homebrew-core/f3a766ef944b1080be64bd88dca9a1d80130c558/Formula/python.rb
+python3 -m venv env
+source env/bin/activate
+pip install -r requirements.txt
+```
+
--- kosher_connections/settings.py
+++ kosher_connections/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_mysql',
+    'drf_generators',  # for django api code generation
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,19 @@ WSGI_APPLICATION = 'kosher_connections.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'social_network',
+        'USER': 'root',
+	'PASSWORD': 'shidduchim',
+        'OPTIONS': {
+            # Tell MySQLdb to connect with 'utf8mb4' character set
+            'charset': 'utf8mb4',
+        },
+        # Tell Django to build the test database with the 'utf8mb4' character set
+        'TEST': {
+            'CHARSET': 'utf8mb4',
+            'COLLATION': 'utf8mb4_unicode_ci',
+        }
     }
 }
 
